f1a6efd9c42f9d2529d06e99f05177ba2bfd0244
Altamash Sayyed <sayyedaltamash853@gmail.com>
2024-02-05 15:41:42 +0530

    tf tradelog index.js rerending fixed

--- src/component/tradeLog/index.js
+++ src/component/tradeLog/index.js
@@ -492,6 +492,7 @@ function TradeLog() {
 
   const handlePrevious = () => {
     //handling function according to reduxData.totalRecords
+    
     if (pageDetail.page > 0) {
       setPageDetail((prev) => ({ ...prev, page: prev.page - 1 }));
       let payloadUrl = `&pageSize=${pageDetail.pageSize}&page=${
